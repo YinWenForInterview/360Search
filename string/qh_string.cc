02d57f3e7ce26858066c10a37b51bff8fa2fd4f7
YW <yinwen1991@gmail.com>
2015-09-04 20:56:20 +0800

    完成编码，还未添加测试用例

--- string/qh_string.cc
+++ string/qh_string.cc
@@ -9,29 +9,48 @@ namespace qh
     string::string()
         : data_(NULL), len_(0)
     {
+        data_ = new char[1];
+        data_[0] = '\0';
+
     }
 
     string::string( const char* s )
     {
+        len_ = strlen(s);
+        data_ = new char[len_ +1];
+        strcpy(data_, s);
     }
 
     string::string( const char* s, size_t len )
     {
+        len_ = len;
+        data_ = new char[len_+1];
+        strncpy(data_, s, len_);
+        data_[len_] = '\0';
     }
 
     string::string( const string& rhs )
     {
-
+        len_ = rhs.len_;
+        data_ = new char[len_+1];
+        strcpy(data_, rhs.data_);
     }
 
     string& string::operator=( const string& rhs )
     {
+        if(this == &rhs)
+            return *this;
+
+        delete [] data_;
+        len_ = rhs.len_ ;
+        data_ = new char[len_ +1];
+        strcpy(data_, rhs.data_);
         return *this;
     }
 
     string::~string()
     {
-
+        delete [] data_;
     }
 
     size_t string::size() const
@@ -41,7 +60,7 @@ namespace qh
 
     const char* string::data() const
     {
-        return NULL;
+        return data_;
     }
 
     const char* string::c_str() const
@@ -51,6 +70,6 @@ namespace qh
 
     char* string::operator[]( size_t index )
     {
-        return NULL;
+        return &data_[index];
     }
 }
